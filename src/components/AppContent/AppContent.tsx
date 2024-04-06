1fdacf2442612f947b53e82fd6f1a4160dab47cc
Trung Cao Sỹ <trung.cs@teko.vn>
2024-04-06 17:56:06 +0700

    Show Super App extra data from mini-app

--- src/components/AppContent/AppContent.tsx
+++ src/components/AppContent/AppContent.tsx
@@ -6,14 +6,27 @@ import { SuperAppConstant } from "../../constant/constant";
 
 const AppLayout: React.FC = () => {
 
-  const onPay = () => {
+  const onFinish = () => {
     getSupperAppModule(window)?.invoke(SuperAppConstant.SuperAppFinisAppCommand, "", "");
+  }
+
+  function getExtraData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const result : string[] = [];
+    urlParams.forEach((value, key, _) => {
+      result.push(`${ key }: ${ value }`);
+    });
+
+
+    return result.map(person => <p>{person}</p>);
   }
 
   return (
     <Layout className="app-layout-container">
-      <Button type={"link"} size={"large"} onClick={onPay}>Finish Mini App</Button>
-      <Button type={"link"} size={"large"} onClick={onPay}>Show Extra Config From Super App</Button>
+      <h2>Extra Data from Super-App</h2>
+      {getExtraData()}
+      <h2>Command</h2>
+      <Button type={"primary"} size={"large"} onClick={onFinish}>Finish Mini App</Button>
     </Layout>
   );
 };
