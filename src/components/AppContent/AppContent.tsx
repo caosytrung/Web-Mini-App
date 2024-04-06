20360a0d96f5d01793a25eaf3b0de0991b3e719c
Trung Cao Sỹ <trung.cs@teko.vn>
2024-04-06 17:05:50 +0700

    Add Finish Mini App Command

--- src/components/AppContent/AppContent.tsx
+++ src/components/AppContent/AppContent.tsx
@@ -2,22 +2,25 @@ import { Button, Layout } from "antd";
 import './AppContent.scss'
 import React from "react";
 
+
 const AppLayout: React.FC = () => {
 
+  const onPay = () => {
+    getSupperAppModule(window)?.invoke("finishMiniApp", "", "");
+  }
 
   return (
     <Layout className="app-layout-container">
-      <LoginView />
+      <Button type={"link"} size={"large"} onClick={onPay}>Finish Mini App</Button>
+      <Button type={"link"} size={"large"} onClick={onPay}>Show Extra Config From Super App</Button>
     </Layout>
   );
 };
 
-
-const LoginView: React.FC = () => (
-  <Button type={"link"} size={"large"} onClick={() => {
-  }}>Login With Teko</Button>
-
-)
-
+function getSupperAppModule(global: any): any | null {
+  return global['MINI_APP_JS_INTERFACE'];
+}
 
 export default AppLayout;
+
+
